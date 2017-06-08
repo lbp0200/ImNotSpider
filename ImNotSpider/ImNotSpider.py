8c869b333a5f0aad9d5497d7da468227f9fc1cd8
刘博平(焦点) <bopingliu@sohu-inc.com>
2017-06-08 15:03:45 +0800

    init

---
+++ ImNotSpider/ImNotSpider.py
@@ -0,0 +1,31 @@
+class ImNotSpider:
+    def random(self):
+        pass
+
+    def pc(self):
+        pass
+
+    def wap(self):
+        pass
+
+    def pc_linux(self):
+        pass
+
+    def pc_windows(self):
+        pass
+
+    def pc_mac(self):
+        pass
+
+    def android(self):
+        pass
+
+    def iphone(self):
+        pass
+
+    def windows_phone(self):
+        pass
+
+
+if __name__ == 'main':
+    pass
